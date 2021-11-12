9a9ab403b224bd09f70507d37b37830dedeff301
marianneleal <marianneleal93@gmail.com>
2021-11-12 12:11:27 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            centerTitle: true,
+            title: Text('Magic 8 Ball'),
+          ),
+          body: BlackBall(),
+        ),
       ),
     );
+
+class BlackBall extends StatefulWidget {
+  @override
+  _BlackBallState createState() => _BlackBallState();
+}
+
+class _BlackBallState extends State<BlackBall> {
+  int ballNumber = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Center(child: Image.asset('images/ball$ballNumber.png')),
+      onPressed: () {
+        shakeBall();
+      },
+    );
+  }
+}
